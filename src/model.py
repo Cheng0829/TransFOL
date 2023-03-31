f69704c3b7478e92f0fb9ca3722b373e387c55a4
Junkai Cheng <376023877@qq.com>
2023-03-31 14:39:58 +0800

    Add files via upload

--- src/model.py
+++ src/model.py
@@ -10,6 +10,94 @@ from math import sin, cos
 from typing import Optional
 from graph_util import Graph, GraphEmbed, GraphWithAnswer, BatchMatGraph
 
+class GraphConvolution(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        """图卷积：L*X*\theta
+        Args:
+        ----------
+            input_dim: int
+                节点输入特征的维度
+            output_dim: int
+                输出特征维度
+            use_bias : bool, optional
+                是否使用偏置
+        """
+ 
+        super().__init__()
+        self.weight = nn.Parameter(torch.Tensor(input_dim, output_dim),requires_grad=True).cuda()
+        self.bias = nn.Parameter(torch.Tensor(output_dim))
+        self.reset_parameters()
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight)
+        nn.init.zeros_(self.bias)
+    def forward(self, adjacency, input_feature):
+        """邻接矩阵是稀疏矩阵，因此在计算时使用稀疏矩阵乘法
+        Args: 
+        -------
+            adjacency: torch.sparse.FloatTensor
+                邻接矩阵
+            input_feature: torch.Tensor
+                输入特征
+        """
+        """
+        GCN1:
+        input_feature: feat[0]: [7,1024]
+        self.weight: [1024,1710]
+        support: [7,1710]
+        adjacency: [1710,1710]
+        output: [7,1710]
+
+        GCN2:
+        input_feature: [7,1710]
+        self.weight: [1710,1710]
+        support: [7,1710]
+        adjacency: [1710,1710]
+        output: [7,1710]
+        """
+        
+        # GCN1: support: [adjacency_size, hidden_size] * [hidden_size, adjacency_size], 
+        # GCN2: support: [adjacency_size, adjacency_size] * [adjacency_size, hidden_size]
+        support = torch.mm(input_feature, self.weight) 
+        output = torch.sparse.mm(adjacency, support)
+        output = output + self.bias
+        return output # [7,1710]
+
+class GcnNet(nn.Module):
+    def __init__(self, input_dim, adjacency_size, out_dim):
+        super().__init__()
+        """3层GCN"""
+        self.gcn1 = GraphConvolution(input_dim, adjacency_size)
+        self.gcn2 = GraphConvolution(adjacency_size, adjacency_size)
+        self.gcn3 = GraphConvolution(adjacency_size, out_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, adjacency, feat, data):
+        adjacency_matrix = torch.zeros(len(adjacency),feat.size(2), requires_grad=True).cuda()
+        feature = torch.zeros(feat.size(0),feat.size(1)//7*2,feat.size(2), requires_grad=True).cuda()
+        for i in range(len(feat)):
+            feature[i][0] = feat[i][0]
+            feature[i][1] = feat[i][1] # feature: [batch_Size, 2, 1024]
+        feature = feature.view(-1, feat.size(2)) # [batch_Size*2, hidden_size] [4096, 1024]
+        for i in range(len(data.x)//7):
+            e1 = data.x[7*i]
+            adjacency_matrix[e1] = adjacency_matrix[e1] + feature[2*i]
+            e2 = data.x[7*i+1]
+            adjacency_matrix[e2] = adjacency_matrix[e2] + feature[2*i+1] # adjacency_matrix: [num_nodes, hidden_size]
+
+        logits = self.relu(self.gcn1(adjacency, adjacency_matrix)) # [num_nodes, num_nodes]
+        logits = self.gcn2(adjacency, logits) # [num_nodes, hidden_size]
+        logits = self.gcn3(adjacency, logits) # [num_nodes, hidden_size]
+        
+        for i in range(len(data.x)//7):
+            e1 = data.x[7*i]
+            feature[2*i] = logits[e1]
+            e2 = data.x[7*i+1]
+            feature[2*i+1] = logits[e2] # adjacency_matrix: [num_nodes, hidden_size]
+        feature = feature.view(-1, 2, feature.size(1))
+        feat[::, 0, ::] = feature[::, 1, ::]
+        feat[::, 1, ::] = feature[::, 0, ::]
+        return feat
+
 class TokenEmbedding(Module):
     r"""
     Hold separate embeddings for different types of tokens and adds type embeddings to the tokens.
@@ -36,7 +124,7 @@ class TokenEmbedding(Module):
                 '''item = [1710,512], [1,512], [86,512]'''
                 item = torch.nn.Embedding(item, embed_dim) 
                 """**********************************************************************************************"""
-                item = torch.nn.Embedding(14429, embed_dim)
+                # item = torch.nn.Embedding(14429, embed_dim)
                 """**********************************************************************************************"""
             elif isinstance(item, torch.nn.Embedding):
                 assert item.embedding_dim == embed_dim
@@ -71,15 +159,19 @@ class TokenEmbedding(Module):
             """三元组变换后把关系也当作实体表示, 所以和首尾实体共用一个嵌入矩阵"""
             feat[mask] += embed(node_id[mask]) # [3584,512]
             """"""
+        
         j = 0
-        while j < len(node_type)/4:
-            break
+        while j < len(node_type)/7:
+            # break
             """聚集关系数据,因此不能用在DDI等单关系实验上"""
-            e_1 = node_id[4*j]
-            r_1 = node_id[4*j+2]
-            r_2 = node_id[4*j+3]
-            if max(e_1,r_1,r_2) < len(feat)//4:
-                pass
+            e_1 = node_id[7*j]
+            e_2 = node_id[7*j+1]
+            r_1 = node_id[7*j+3]
+            r_2 = node_id[7*j+4]
+            r_3 = node_id[7*j+5]
+            r_4 = node_id[7*j+6]
+            if max(e_1,e_2,r_1,r_2,r_3,r_4) < len(feat)//7:
+
                 """新MSTE Sin"""
                 # tmp_e_1 = torch.Tensor.detach(torch.sin(0.5*(feat[r_1]+feat[r_3]) * feat[e_2])) * feat[e_1]
                 # tmp_e_2 = torch.Tensor.detach(torch.sin(0.5*(feat[r_4]+feat[r_2]) * feat[e_1])) * feat[e_2]
@@ -139,6 +231,55 @@ class TokenEmbedding(Module):
             j = j + 1
 
         return feat
+
+class Match(nn.Module):
+    def __init__(self, hidden_size): # hidden_size=400
+        super().__init__()
+        self.trans_linear = nn.Linear(hidden_size, hidden_size).cuda()
+        self.map_linear = nn.Linear(2*hidden_size, 2*hidden_size).cuda()
+        self.relu = nn.ReLU().cuda()
+    def forward(self, proj_p, proj_q):
+        # proj_p, proj_q: [hidden_size,]
+        # 一个FC层
+        # '''论文中的双向注意力机制'''
+        trans_q = self.trans_linear(proj_q)
+        att_weights = proj_p * trans_q# proj_p.bmm(torch.transpose(trans_q, 1, 2))
+        att_norm = torch.nn.functional.softmax(att_weights, dim=-1)
+        att_vec = att_norm * proj_q # .bmm(proj_q)
+
+        # trans_q = self.trans_linear(proj_q)
+        # att_weights = proj_p.bmm(torch.transpose(trans_q, 0, 1))
+        # att_norm = torch.nn.functional.softmax(att_weights, dim=-1)
+        # att_vec = att_norm.bmm(proj_q)
+
+        '''公式7/8'''
+        elem_min = att_vec - proj_p
+        elem_mul = att_vec * proj_p
+        all_con = torch.cat([elem_min, elem_mul])
+        output = self.relu(self.map_linear(all_con))
+        return output # Ger 2*hidden_size
+
+class HighwayNetwork(nn.Module):
+    def __init__(self, hidden_size, with_sigmoid=False):
+        super().__init__()
+        self.linear = nn.Linear(2*hidden_size, hidden_size).cuda()
+        self.linear2 = nn.Linear(2*hidden_size, 2*hidden_size).cuda()
+        self.linear3 = nn.Linear(2*hidden_size, 2*hidden_size).cuda()
+        self.with_sigmoid =  with_sigmoid
+        self.sigmoid = nn.Sigmoid().cuda()
+    def forward(self, p, q):
+        # inputs: Ger Ges -> embedding
+        lp = self.linear2(p) # Ger
+        lq = self.linear3(q) # Ges
+        '''公式9'''
+        sigma = self.sigmoid(lp+lq) # 门控单元
+        '''公式10'''
+        output = sigma*p + (1-sigma)*q # Ge
+        '''公式11'''
+        output = self.linear(output) # He
+        if self.with_sigmoid:
+            output = torch.sigmoid(output).cuda()
+        return output
 
 class FeedForwardNetwork(nn.Module):
     def __init__(self, hidden_size, ffn_size, dropout_rate):
@@ -214,16 +355,16 @@ class entity_EncoderLayer(nn.Module):
         self.ffn = FeedForwardNetwork(hidden_size, ffn_size, dropout_rate)
         self.ffn_norm = nn.LayerNorm(hidden_size)
         self.ffn_dropout = nn.Dropout(dropout_rate)
-    #     self.attn_bias = nn.Parameter(torch.Tensor(2,2))
-    #     self.reset_parameters()
-    # def reset_parameters(self):
-    #     nn.init.zeros_(self.attn_bias)
+        self.attn_bias = nn.Parameter(torch.Tensor(2,2))
+        self.reset_parameters()
+    def reset_parameters(self):
+        nn.init.zeros_(self.attn_bias)
 
     def forward(self, x, attn_bias=None):
         # x, y: [1024, 7, 1024] [batch_size, 7, hidden_size]
         y = self.self_attention_norm(x)
-        y = self.self_attention(y, y, y, attn_bias)
-        # y[::, 0:2, ::] = self.self_attention(y[::, 0:2, ::], y[::, 0:2, ::], y[::, 0:2, ::], attn_bias=self.attn_bias)
+        # y = self.self_attention(y, y, y, attn_bias)
+        y[::, 0:2, ::] = self.self_attention(y[::, 0:2, ::], y[::, 0:2, ::], y[::, 0:2, ::], attn_bias=self.attn_bias)
         # y[::, 3:, ::] = self.self_attention(y[::, 3:, ::], y[::, 3:, ::], y[::, 3:, ::], attn_bias=None)
         y = self.self_attention_dropout(y)
         x = x + y # 残差
@@ -242,16 +383,15 @@ class relation_EncoderLayer(nn.Module):
         self.ffn = FeedForwardNetwork(hidden_size, ffn_size, dropout_rate)
         self.ffn_norm = nn.LayerNorm(hidden_size)
         self.ffn_dropout = nn.Dropout(dropout_rate)
-    #     self.attn_bias = nn.Parameter(torch.Tensor(2,2))
-    #     self.reset_parameters()
-    # def reset_parameters(self):
-    #     nn.init.zeros_(self.attn_bias)
-
+        self.attn_bias = nn.Parameter(torch.Tensor(4,4))
+        self.reset_parameters()
+    def reset_parameters(self):
+        nn.init.zeros_(self.attn_bias)
     def forward(self, x, attn_bias=None):
         # x, y: [1024, 7, 1024] [batch_size, 7, hidden_size]
         y = self.self_attention_norm(x)
-        y = self.self_attention(y, y, y, attn_bias)
-        # y[::, 2:, ::] = self.self_attention(y[::, 2:, ::], y[::, 2:, ::], y[::, 2:, ::], attn_bias=self.attn_bias)
+        # y = self.self_attention(y, y, y, attn_bias)
+        y[::, 3:, ::] = self.self_attention(y[::, 3:, ::], y[::, 3:, ::], y[::, 3:, ::], attn_bias=self.attn_bias)
         y = self.self_attention_dropout(y)
         x = x + y # 残差
         y = self.ffn_norm(x) # 正则化
@@ -272,16 +412,15 @@ class cross_entity_EncoderLayer(nn.Module):
         self.ffn_attention_norm = nn.LayerNorm(hidden_size)
         self.ffn = FeedForwardNetwork(hidden_size, ffn_size, dropout_rate)
         self.ffn_dropout = nn.Dropout(dropout_rate)
-    #     self.attn_bias = nn.Parameter(torch.Tensor(2,2))
-    #     self.reset_parameters()
-    # def reset_parameters(self):
-    #     nn.init.zeros_(self.attn_bias)
+        self.attn_bias = nn.Parameter(torch.Tensor(2,2))
+        self.reset_parameters()
+    def reset_parameters(self):
+        nn.init.zeros_(self.attn_bias)
 
     def forward(self, x, attn_bias=None):
         '''多头注意力1'''
         y = self.attention_norm_first(x)
-        y = self.cross_self_attention(y, y, y, attn_bias)
-        # y[::, 0:2, ::] = self.cross_self_attention(y[::, 0:2, ::], y[::, 2:, ::], y[::, 1:3, ::], attn_bias=self.attn_bias)
+        y[::, 0:2, ::] = self.cross_self_attention(y[::, 0:2, ::], y[::, 3:5, ::], y[::, 5:, ::], attn_bias=self.attn_bias)
         y = self.attention_dropout_first(y)
         x = x + y # 残差
         '''多头注意力2''' 
@@ -308,16 +447,15 @@ class cross_relation_EncoderLayer(nn.Module):
         self.ffn_attention_norm = nn.LayerNorm(hidden_size)
         self.ffn = FeedForwardNetwork(hidden_size, ffn_size, dropout_rate)
         self.ffn_dropout = nn.Dropout(dropout_rate)
-    #     self.attn_bias = nn.Parameter(torch.Tensor(2,2))
-    #     self.reset_parameters()
-    # def reset_parameters(self):
-    #     nn.init.zeros_(self.attn_bias)
+        self.attn_bias = nn.Parameter(torch.Tensor(2,2))
+        self.reset_parameters()
+    def reset_parameters(self):
+        nn.init.zeros_(self.attn_bias)
 
     def forward(self, x, attn_bias=None):
         '''多头注意力1'''
         y = self.attention_norm_first(x)
-        y = self.cross_self_attention(y, y, y, attn_bias)
-        # # y[::, 2:, ::] = self.cross_self_attention(y[::, 2:, ::], y[::, 0:2, ::], y[::, 0:2, ::], attn_bias=None)
+        y[::, 3:5, ::] = self.self_attention(y[::, 3:5, ::], y[::, 5:, ::], y[::, 0:2, ::], attn_bias=self.attn_bias)
         y = self.attention_dropout_first(y)
         x = x + y # 残差
         '''多头注意力2''' 
@@ -358,6 +496,7 @@ class D_KGTransformer(Module):
         with torch.no_grad():
             self.attn_bias_embed.weight[1] = torch.full((self.num_heads,), float('-inf'))
 
+
         self.entity_encode_layers = nn.ModuleList([
             entity_EncoderLayer(
                 hidden_size=config['hidden_size'],
@@ -366,7 +505,7 @@ class D_KGTransformer(Module):
                 attention_dropout_rate=config['attention_dropout'],
                 head_num=config['num_heads']
             )
-            for _ in range(1)
+            for _ in range(2)
         ])
         self.relation_encode_layers = nn.ModuleList([
             relation_EncoderLayer(
@@ -387,7 +526,7 @@ class D_KGTransformer(Module):
                 attention_dropout_rate=config['attention_dropout'],
                 head_num=config['num_heads']
             )
-            for _ in range(3)
+            for _ in range(8)
         ])
 
         self.cross_relation_encode_layers = nn.ModuleList([
@@ -398,19 +537,19 @@ class D_KGTransformer(Module):
                 attention_dropout_rate=config['attention_dropout'],
                 head_num=config['num_heads']
             )
-            for _ in range(1)
+            for _ in range(0)
         ])
 
-        self.cat_linear = torch.nn.Linear(config['hidden_size'], config['hidden_size']).cuda()
+        self.cat_linear = torch.nn.Linear(config['hidden_size'], config['hidden_size'])
 
         """GCN"""
-        # self.adjacency = torch.zeros(num_nodes, num_nodes).cuda()
-        # self.gcnnet = GcnNet(input_dim=config['hidden_size'], adjacency_size=num_nodes, out_dim=config['hidden_size']) # 
+        self.adjacency = torch.zeros(num_nodes, num_nodes).cuda()
+        self.gcnnet = GcnNet(input_dim=config['hidden_size'], adjacency_size=num_nodes, out_dim=config['hidden_size']) # 
 
         self.final_ln = nn.LayerNorm(self.d_model)
         self.pred_ent_proj = torch.nn.Linear(self.d_model, self.num_nodes)
         self.loss_type = config['loss']
-        self.smoothing = config['smoothing']
+        self.smoothing = config['smoothing']        
 
     def forward(self, data: BatchMatGraph):
         # self.attn_bias_embed: [40,8]    data.attn_bias_type: [1024,7,7]
@@ -434,9 +573,9 @@ class D_KGTransformer(Module):
             feat_r = layer(feat_r)
 
         """cat"""
-        # feat = torch.cat([feat_e[::,0:2,::], feat_r[::,2:,::]], dim=1).cuda()
-        # feat_e = feat
-        # feat_r = feat
+        feat = torch.cat([feat_e[::,0:3,::], feat_r[::,3:,::]], dim=1)
+        feat_e = feat
+        feat_r = feat
 
         """cross Encoder + Encoder"""
         for layer in self.cross_entity_encode_layers:
@@ -445,7 +584,19 @@ class D_KGTransformer(Module):
             feat_r = layer(feat_r, attn_bias)
 
         """cat"""
-        feat = self.cat_linear(torch.cat([feat_e[::,0:2,::], feat_r[::,2:,::]], dim=1)).cuda()
+        feat = self.cat_linear(torch.cat([feat_e[::,0:3,::], feat_r[::,3:,::]], dim=1))
+
+        '''*****************************************************************************************************************'''
+
+        """GCN"""
+        for i in range(0, len(data.x), 7):
+            row = data.x[i]
+            column = data.x[i+1]
+            self.adjacency[row][column] = 1
+        feat_gcn = self.gcnnet(self.adjacency, feat, data)
+        feat = feat_gcn
+        
+        '''*****************************************************************************************************************'''
         
         feat = self.final_ln(feat)
         feat = feat.view(-1, self.d_model)
